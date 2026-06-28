db2ce4c5a585ecef53d37b7a6a8ea13b701398b6
yacinemeli06 <yacinemeli06@gmail.com>
2026-06-28 23:04:05 +0200

    Add files via upload

--- formulaire.sql
+++ formulaire.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : dim. 28 juin 2026 à 22:04
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
